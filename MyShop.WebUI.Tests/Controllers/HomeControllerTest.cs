b1c065f205290e22cdde20db62d3c87716038b5b
Urvish-28 <ubutani@aakashinformatics.com>
2023-02-23 18:28:33 +0530

    main error solve

--- MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -15,14 +15,7 @@ namespace MyShop.WebUI.Tests.Controllers
         [TestMethod]
         public void Index()
         {
-            //// Arrange
-            //HomeController controller = new HomeController();
-
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
-
-            //// Assert
-            //Assert.IsNotNull(result);
+            
         }
     }
 }
