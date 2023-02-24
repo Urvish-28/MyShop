55da83976f37a3eddaf1c4e25d131b415797b443
Urvish-28 <ubutani@aakashinformatics.com>
2023-02-24 17:04:15 +0530

    link customer to order

--- WebApplication1/Controllers/BasketController.cs
+++ WebApplication1/Controllers/BasketController.cs
@@ -10,13 +10,15 @@ namespace WebApplication1.Controllers
 {
     public class BasketController : Controller
     {
+        IRepository<Customer> customers;
         IBasketService basketService;
         IOrderService orderService;
 
-        public BasketController(IBasketService BasketService , IOrderService OrderService)
+        public BasketController(IBasketService BasketService , IOrderService OrderService , IRepository<Customer> Customers)
         {
             this.basketService = BasketService;
             this.orderService = OrderService;
+            this.customers = Customers;
         }
 
         // GET: Basket
@@ -47,16 +49,40 @@ namespace WebApplication1.Controllers
             return PartialView(basketSummary);
         }
 
+        [Authorize]
         public ActionResult Checkout()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+
+            if(customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    Surname = customer.LastName,
+                    ZipCode = customer.ZipCode
+                };
+
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+           
         }
 
         [HttpPost]
+        [Authorize]
         public ActionResult Checkout(Order order)
         {
             var basketitems = basketService.GetBasketItem(this.HttpContext);
             order.OrderStatus = "Order Ceated";
+            order.Email = User.Identity.Name;
 
             //process payment
 
